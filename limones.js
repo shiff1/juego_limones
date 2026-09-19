b3e7e554fc9c14a3c8a615e5e6a982f77f4dc505
shiff1 <sl5414841@gmail.com>
2026-09-19 17:41:04 -0500

    aumentar altura personaje y altura suelo

--- limones.js
+++ limones.js
@@ -1,7 +1,19 @@
 let canvas=document.getElementById("areaJuego");
 let ctx=canvas.getContext("2d");
 
+ const ALTURA_SUELO=20;
+ const ALTURA_PERSONAJE=60;
+
+function iniciar(){
+    dibujarSuelo();
+    dibujarPersonaje();
+}
+
 function dibujarSuelo() {
     ctx.fillStyle="blue";
-    ctx.fillRect(0,0,canvas.Width,20)
+    ctx.fillRect(0,canvas.height-ALTURA_SUELO,canvas.width,ALTURA_SUELO);
+}
+function dibujarPersonaje(){
+    ctx.fillStyle="yellow";
+    ctx.fillRect(canvas.width/2,canvas.height-(ALTURA_SUELO+ALTURA_PERSONAJE),40,ALTURA_PERSONAJE);
 }
