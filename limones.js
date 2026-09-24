a9ecdcd77639154311fedfd94f9a02b01bb6bb5a
shiff1 <sl5414841@gmail.com>
2026-09-24 18:48:30 -0500

    posicion aleatorio del limon

--- limones.js
+++ limones.js
@@ -14,7 +14,7 @@ let limonY = 0;
 function iniciar(){
     dibujarSuelo();
     dibujarPersonaje();
-    dibujarLimon();
+    aparecerLimon();
 }
 
 function dibujarSuelo(){
@@ -66,6 +66,18 @@ function detectarColision(){
         limonX < personajeX+ANCHO_PERSONAJE &&
         limonY+ALTURA_LIMON>personajeY && 
         limonY < personajeY+ALTURA_PERSONAJE) {
-       alert("ATRAPADO!!"); 
+        //alert("ATRAPADO!!");
+        aparecerLimon();
     }
+}
+
+function probarAleatorio(){
+    let aleatorio=generarAleatorio(10,80);
+    console.log(aleatorio);
+}
+
+function aparecerLimon(){
+    limonX=generarAleatorio(0,canvas.width-ANCHO_LIMON);
+    limonY=0;
+    actualizarPantalla();
 }
